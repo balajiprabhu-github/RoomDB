e014d09ceb792022022270451616e82e6367c7d2
balajiprabhu-github <balajiprabhu008@gmail.com>
2020-03-21 19:44:46 +0530

    Added a Room Database

---
+++ app/src/main/java/com/balajiprabhu/roomdb/WordRoomDatabase.kt
@@ -0,0 +1,33 @@
+package com.balajiprabhu.roomdb
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class],version = 1,exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase(){
+
+    abstract fun WordDao():WordDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
